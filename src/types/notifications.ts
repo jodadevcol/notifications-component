9ffd46757adf8c966d8caba4a90f63388eff9da3
Danni Garcia <jodadev.col@gmail.com>
2024-10-08 19:19:27 +1100

    Add notifications data and types

---
+++ src/types/notifications.ts
@@ -0,0 +1,24 @@
+export interface Notifications {
+	data: Notification[]
+}
+
+export interface Notification {
+	id: number
+	user: User
+	action: Action
+	date: string
+	status: "unread" | "read"
+	type?: string
+}
+
+export interface User {
+	name: string
+	avatar: string
+}
+
+export interface Action {
+	type: string
+	details: string
+	message?: string
+	post?: string
+}
